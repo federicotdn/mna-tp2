f28c18ac574d68f21450a419eea2e8fc57fc7834
Javier Fraire <javier.fraire@wolox.com.ar>
2014-11-14 01:33:48 -0300

    no anda

---
+++ src/huffman.py
@@ -0,0 +1,33 @@
+from heapq import heappush, heappop, heapify
+from collections import defaultdict
+from collections import Counter
+
+
+def encode(symb2freq):
+    if len(list(symb2freq.keys())) == 1:
+        return {list(symb2freq.keys())[0]: '0'}
+    heap = [[wt, [sym, ""]] for sym, wt in list(symb2freq.items())]
+    heapify(heap)
+    while len(heap) > 1:
+        lo = heappop(heap)
+        hi = heappop(heap)
+        for pair in lo[1:]:
+            pair[1] = '0' + pair[1]
+        for pair in hi[1:]:
+            pair[1] = '1' + pair[1]
+        heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
+    return dict(sorted(heappop(heap)[1:], key=lambda p: (len(p[-1]), p)))
+
+
+def estimate(v):
+    counter = Counter(v)
+    huff_dict = encode(counter)
+    total_bits = 0
+    symbols = list(set(v))
+    total_bits += len(symbols)
+    for symbol in symbols:
+        total_bits += len(huff_dict[symbol])
+
+    for k, v in list(counter.items()):
+         total_bits += len(huff_dict[k])*v
+    return total_bits
--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
-import wave, cmath, math
+import wave, cmath, math, numpy, sys, huffman
 
 def main():
 	f = wave.open('audio/corto2.wav')
 	N = f.getnframes()
 	ks = []
+	epsilon = 0.1
+	L = 8
+	max_size = math.pow(2,L) - 1
 	
 
 	for i in range(N):
@@ -11,14 +14,98 @@ def main():
 
 	for i in range(10):
 		print(ks[i])
+	print('------------------------------------------------------------')
 
-	alphas = dft(ks)
-	data = inverse_dft(alphas, N)
+	alphas = numpy.fft.fft(ks)
+	n = math.floor(N/2) + 1
+	alphas = alphas[:n]
 
-	print('------------------------------------------------')
-	for i in range(10):
-		print(round(abs(data[i])))
+	for i, alpha in enumerate(alphas):
+		if (abs(alpha) < epsilon):
+			alphas[i] = 0
+
+	min_val, max_val = get_max_and_min(alphas)
 	
+	real_alphas = []
+	imag_alphas = []
+	fn  = lambda x: int(((x - min_val)/(max_val - min_val))* max_size)
+
+	for alpha in alphas:
+		real_alphas.append(fn(alpha.real))
+		imag_alphas.append(fn(alpha.imag))
+
+	
+
+	# compressed_bits = huffman.estimate(real_alphas + imag_alphas)
+	# print('Compressed: ' + str(compressed_bits))
+	# print('Uncompressed: ' + str(N*16))
+
+	fn = lambda x: ((x/max_size)*(max_val - min_val)) + min_val
+	new_alphas = []
+	for i in range(n):
+		new_alphas.append(complex(fn(real_alphas[i]), fn(imag_alphas[i])))
+
+	print('------------------------------------------------------------')
+	for i in range(10):
+		print('old: ' + str(alphas[i]) + ' new: ' + str(new_alphas[i]))
+
+	output = wave.open('audio/output.wav', 'wb')
+	output.setparams(f.getparams())
+	new_alphas.extend(reversed(new_alphas[1:]))
+	inverse_fft = numpy.fft.ifft(new_alphas)
+
+	for i in range(10):
+		print(int(inverse_fft[i].real))
+
+	bytes = []
+	for num in inverse_fft:
+		bytes.append(int(abs(num.real)).to_bytes(2, byteorder = 'little'))
+
+	output.writeframes(bytes)
+
+
+	# alphas = dft(ks)
+	# max_real = max((n.real for n in alphas))
+	# min_real = min((n.real for n in alphas))
+	# max_imag = max((n.imag for n in alphas))
+	# min_imag = min((n.imag for n in alphas))
+
+	# print('max real ' + str(max_real))
+	# print('min real ' + str(min_real))
+	# print('max imag ' + str(max_imag))
+	# print('min imag '  + str(min_imag))
+
+	# for i in range(len(alphas)):
+	# 	aux = alphas[i]
+	# 	# alpha = complex((alphas[i].real*math.pow(2,3))/(max_real - min_real), (alphas[i].imag*math.pow(2,3))/(max_imag - min_imag))
+	# 	alpha = complex((alphas[i].real - min_real) /(max_real - min_real))
+	# 	alphas[i] = alpha
+	# 	print(str(alpha) + '  vs  ' +  str(aux))
+
+
+	# data = inverse_dft(alphas, N)
+
+	# print('------------------------------------------------')
+	# for i in range(10):
+	# 	print(round(abs(data[i])))
+	
+
+def get_max_and_min(v):
+	min_val = v[0].real
+	max_val = v[0].real
+
+	for num in v:
+		if (num.real < min_val):
+			min_val = num.real
+		elif (num.real > max_val):
+			max_val = num.real
+
+		if (num.imag < min_val):
+			min_val = num.imag
+		elif (num.imag > max_val):
+			max_val = num.imag
+
+	return min_val, max_val
 
 def dft(ks):
 	N = len(ks)
@@ -87,7 +174,7 @@ def inverse_fft(v):
             result[k] = x
             result[k + n//2] = y
         return result
-    return [x.real/len(v) for x in recursive_inverse_fft(v)]
+    return (x.real/len(v) for x in recursive_inverse_fft(v))
 	
 	
 if __name__ == '__main__':
